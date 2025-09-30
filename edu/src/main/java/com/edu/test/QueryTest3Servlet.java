2d06ed9418a7d8f18403b8a6dbe2b009d8adf43a
Chanwoo1124 <lgw22774@gmail.com>
2025-09-30 23:15:16 +0900

    session,cookie,context

---
+++ edu/src/main/java/com/edu/test/QueryTest3Servlet.java
@@ -0,0 +1,47 @@
+package com.edu.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/queryTest3")
+public class QueryTest3Servlet extends HttpServlet{
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,IOException{
+		resp.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = resp.getWriter();
+		out.print("<html>");
+		out.print("<body>");
+		
+		String name = req.getParameter("name");
+		out.print(name+"<br>");
+		out.print("</body></html>");
+		out.close();
+	}
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) 
+															throws ServletException,IOException{
+		req.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = resp.getWriter();
+		out.print("<html>");
+		out.print("<body>");
+			
+		String name = req.getParameter("name");
+		out.print(name+"<br>");
+		out.print("</body></html>");
+		out.close();
+			
+		
+
+		
+		
+		
+		 
+		
+	}
+	
+}
--- edu/src/main/java/com/edu/test/QueryTestServlet.java
+++ edu/src/main/java/com/edu/test/QueryTestServlet.java
@@ -1,5 +1,43 @@
 package com.edu.test;
 
-public class QueryTestServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/queryTest")
+public class QueryTestServlet extends HttpServlet {
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			resp.setContentType("text/html,charset=UTF-8");
+			PrintWriter out = resp.getWriter();
+			out.print("<htm>");
+			out.print("<body>");
+			out.print("get요청으로 왔습니");
+			out.print("</body>");
+			out.print("</html>");
+			out.close();
+		
+		
+	}
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html,charset=UTF-8");
+		PrintWriter out = resp.getWriter();
+		out.print("<htm>");
+		out.print("<body>");
+		out.print("post요청으로 왔습니다");
+		out.print("</body>");
+		out.print("</html>");
+		out.close();
+		
+	}
 
 }
+
+
